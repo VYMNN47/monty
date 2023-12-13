88482328f3ecc2e898f599d7634b79018a99b5ab
Ahmed <ahmed_l2@yahoo.com>
2023-12-14 02:25:42 +0300

    updated

--- main.c
+++ main.c
@@ -16,27 +16,26 @@ int main(int argc, char **argv)
 	char *buff = NULL;
 	size_t buff_len = 0;
 	unsigned int line_num = 1;
+	ssize_t read_line = 1;
 	stack_t *stack = NULL;
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "USAGE: monty file\n"),	exit(EXIT_FAILURE);
 	}
 	filem = fopen(argv[1], "r");
 	if (!filem || !argv[1])
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]), exit(EXIT_FAILURE);
 	}
-	while (getline(&buff, &buff_len, filem) != -1)
+	while (read_line > 0)
 	{
+		read_line = getline(&buff, &buff_len, filem);
 		if (*buff == '\n')
 		{
 			line_num++;
 			continue;
 		}
-
 		info.op = strtok(buff, " \t\n");
 		if (!info.op || *info.op == '#')
 		{
@@ -44,7 +43,8 @@ int main(int argc, char **argv)
 			continue;
 		}
 		info.arg = strtok(NULL, " \t\n");
-		exec(&stack, line_num);
+		if (read_line > 0)
+			exec(&stack, line_num);
 		line_num++;
 	}
 	free(buff);
