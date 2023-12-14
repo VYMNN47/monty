bf99fe10e0325ffb8144649c5483d245f496ee73
Ahmed <ahmed_l2@yahoo.com>
2023-12-14 03:23:57 +0300

    9. mod

--- op_functions2.c
+++ op_functions2.c
@@ -12,7 +12,7 @@ void mod(stack_t **stack, unsigned int line_number)
 
 	if (!*stack || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->n == 0)
