adfd7ab46cf1fa40e4b9c82e3661f6edb803921d
Ahmed <ahmed_l2@yahoo.com>
2023-12-14 20:33:51 +0300

    .

--- op_functions2.c
+++ op_functions2.c
@@ -32,9 +32,6 @@ void pchar(stack_t **stack, unsigned int line_number)
 	if (!*stack)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
-		fclose(info.file);
-                free(info.buff);
-                free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
 
@@ -42,9 +39,6 @@ void pchar(stack_t **stack, unsigned int line_number)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n",
 				line_number);
-                fclose(info.file);
-                free(info.buff);
-                free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
 
