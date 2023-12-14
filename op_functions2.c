a481cfc74aba9059b5d5c781fb154cab79324b55
Ahmed <ahmed_l2@yahoo.com>
2023-12-14 19:27:43 +0300

    11. pchar

--- op_functions2.c
+++ op_functions2.c
@@ -29,13 +29,13 @@ void mod(stack_t **stack, unsigned int line_number)
 
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	if (!*stack || !stack)
+	if (!*stack)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	if ((*stack)->n < 0 || (*stack)->n > 127)
+	if ((*stack)->n <= 0 || (*stack)->n > 127)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n",
 				line_number);
