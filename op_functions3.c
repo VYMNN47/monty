8c55d2768fbb9e2b2f9ac8a9bcc9f16cd3d1b090
aymnn <ayman.belahsen.4@gmail.com>
2023-12-15 01:09:19 +0100

    .

--- op_functions3.c
+++ op_functions3.c
@@ -28,7 +28,7 @@ void pushq(stack_t **stack, unsigned int line_num)
 	if (new_node == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
-	  	exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 	new_node->n = value, new_node->next = NULL;
 	if (temp)
