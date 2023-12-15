f29b05f897cca2b9bf3730827b1971c78faa94eb
aymnn <ayman.belahsen.4@gmail.com>
2023-12-15 01:05:22 +0100

    .

--- op_functions3.c
+++ op_functions3.c
@@ -63,9 +63,9 @@ void queue(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * stack_l - Sets the stack format to stack mode (LIFO).
- * @stack: A pointer to the stack (unused in this function).
- * @line_number: The line number associated with the stack_l operation (unused in this function).
+ * stack_l - Sets the stack format to stack mode.
+ * @stack: A pointer to the stack.
+ * @line_number: The line number associated with the stack_l operation
  *
  */
 
